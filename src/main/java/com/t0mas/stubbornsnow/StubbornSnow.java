15b7957e8ad5a1995c9b175e7e77ca78a9358d33
tomasitobambito <tomgacek0604@gmail.com>
2021-06-06 13:40:29 +0200

    initial setup

---
+++ src/main/java/com/t0mas/stubbornsnow/StubbornSnow.java
@@ -0,0 +1,10 @@
+package com.t0mas.stubbornsnow;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StubbornSnow implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
